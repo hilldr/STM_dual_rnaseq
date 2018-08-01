694499c211fc115620f40382897979fd5eca833f
David R. Hill <d2.david.hill@gmail.com>
2018-08-01 16:59:17 -0400

    fix[data]: correct ftp paths for rsync genome download

--- src/download_genomes.sh
+++ src/download_genomes.sh
@@ -9,15 +9,15 @@ mkdir -p ../data/genomes
 
 ## Salmonella enterica subsp. enterica serovar Enteritidis str. P125109
 ## GenBank: AM933172.1
-rsync --copy-links --times --verbose rsync://ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Salmonella_enterica/latest_assembly_versions/GCF_000009505.1_ASM950v1/GCF_000009505.1_ASM950v1_cds_from_genomic.fna.gz ../data/genomes
+rsync --copy-links --times --verbose rsync://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Salmonella_enterica/latest_assembly_versions/GCF_000009505.1_ASM950v1/GCF_000009505.1_ASM950v1_cds_from_genomic.fna.gz ../data/genomes
 
 ## Salmonella enterica Subsp. enterica serovar Typhi Ty2
 ## Genbank: AE014613.1
-rsync --copy-links --times --verbose rsync://ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Salmonella_enterica/latest_assembly_versions/GCF_000007545.1_ASM754v1/GCF_000007545.1_ASM754v1_cds_from_genomic.fna.gz ../data/genomes
+rsync --copy-links --times --verbose rsync://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Salmonella_enterica/latest_assembly_versions/GCF_000007545.1_ASM754v1/GCF_000007545.1_ASM754v1_cds_from_genomic.fna.gz ../data/genomes
 
 ## Salmonella enterica Subsp. enterica serovar Typhimurium SL1344
 ## GenBank: FQ312003.1
-rsync --copy-links --times --verbose rsync://ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Salmonella_enterica/latest_assembly_versions/GCF_000210855.2_ASM21085v2/GCF_000210855.2_ASM21085v2_cds_from_genomic.fna.gz ../data/genomes
+rsync --copy-links --times --verbose rsync://ftp.ncbi.nlm.nih.gov/genomes/refseq/bacteria/Salmonella_enterica/latest_assembly_versions/GCF_000210855.2_ASM21085v2/GCF_000210855.2_ASM21085v2_cds_from_genomic.fna.gz ../data/genomes
 
 for file in ../data/genomes/*.fna.gz
 do
